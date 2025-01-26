b4050e8d574d362381cbbf556ffb7a8558ddcca9
chris mitchell <chris@chrismitchell.xyz>
2025-01-26 14:43:12 +0000

    Revert "Refactor authentication routes: consolidate and move routes from auth.php to web.php"
    This reverts commit 622ce27f232663f4c1534a0bf421ab80611f1227.

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\VerifyEmailController;
-use Livewire\Volt\Volt;
 
+Route::view('/', 'welcome');
 
-Route::get('/', fn () => redirect()->route('login'));
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')
-        ->name('dashboard');
-});
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
-
-Route::middleware('guest')->group(function () {
-    Volt::route('register', 'pages.auth.register')
-        ->name('register');
-
-    Volt::route('login', 'pages.auth.login')
-        ->name('login');
-
-    Volt::route('forgot-password', 'pages.auth.forgot-password')
-        ->name('password.request');
-
-    Volt::route('reset-password/{token}', 'pages.auth.reset-password')
-        ->name('password.reset');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::view('profile', 'profile')
-        ->name('profile');
-
-    Volt::route('verify-email', 'pages.auth.verify-email')
-        ->name('verification.notice');
-
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-        ->middleware(['signed', 'throttle:6,1'])
-        ->name('verification.verify');
-
-    Volt::route('confirm-password', 'pages.auth.confirm-password')
-        ->name('password.confirm');
-});
+require __DIR__.'/auth.php';
